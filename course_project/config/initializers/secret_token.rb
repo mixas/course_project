17e5585216946eed178bd963cc6c32fa804c52c0
mixas <mixas.shel@gmail.om>
2011-10-04 22:05:38 +0300

    initialize

---
+++ course_project/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CourseProject::Application.config.secret_token = '0f6d3d9ee0b7c6f2c69fe6633db5a8fadca63593ae6d72854b699b0135f5020760e086d3548f4a3b3cf052770c91981969cd80225f7709e1f3b833b2c496a126'
