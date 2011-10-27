5495b01b13e7503164fc3f72be254abccbd276ae
mixas <mixas.shel@gmail.om>
2011-10-27 15:15:37 +0300

    users authentification

--- course_project/app/controllers/adaptive_tests_controller.rb
+++ course_project/app/controllers/adaptive_tests_controller.rb
@@ -1,5 +1,6 @@
 class AdaptiveTestsController < ApplicationController
   def index
+    @title = "All Tests"
     @adaptive_tests = AdaptiveTest.all
   end
 end
--- course_project/app/controllers/users_controller.rb
+++ course_project/app/controllers/users_controller.rb
@@ -1,8 +1,11 @@
 class UsersController < ApplicationController
   
+  before_filter :authentificate, :only => [:edit, :update, :show]
+  before_filter :correct_user, :only => [:edit, :update, :show]
+  
   def new
     @user = User.new
-    @title = "sign up"
+    @title = "Sign up"
   end
   
   def create
@@ -11,13 +14,42 @@ class UsersController < ApplicationController
       redirect_to user_path(@user)
     else
       sign_in(@user)
-      @title = "sign up"
+      @title = "Sign up"
       render 'new'
+    end
+  end
+  
+  def update
+    @user = User.find(params[:id])
+    if @user.update_attributes(params[:user])
+      flash[:success] = "Profile updated"
+      redirect_to user_path(@user.id)
+    else
+      @title = 'Edit user'
+      render 'edit'
     end
   end
   
   def show
     @user = User.find(params[:id])
+    @title = "User #{@user.name}"
+  end
+  
+  def edit
+    @user = User.find(params[:id])
+    @title = "Edit user #{@user.name}"
+    render 'edit'
+  end
+  
+  private
+  
+  def authentificate
+    deny_access unless signed_in?
+  end
+  
+  def correct_user
+    @user = User.find(params[:id])
+    redirect_to root_path unless current_user?(@user)
   end
   
 end
